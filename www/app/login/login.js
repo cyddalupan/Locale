5a44ee59cfc112dd89778029969fb004076492dc
markjangeles10 <markjangeles10@gmail.com>
2017-06-11 21:36:27 +0800

    locale-alpha

---
+++ www/app/login/login.js
@@ -0,0 +1,3 @@
+app.controller('apploginController', function($scope) {
+   
+});
--- www/main.js
+++ www/main.js
@@ -1,3 +1,14 @@
-app.controller('mainController', function($scope) {
-	$scope.mainHead = 'This is a Sample global scope'
+app.controller('mainController', function($scope,$location) {
+	$scope.localSettings = false;
+	$scope.localSettings = function() {
+		$scope.localsettingsActive = !$scope.localsettingsActive;
+	};
+
+	$scope.$on('$routeChangeStart', function(next, current) { 
+		if($location.url() == '/login' || $location.url() == '/single-view'){
+			$scope.hideNav = 1;
+		}else{
+			$scope.hideNav = 0;
+		}
+	});
 });
