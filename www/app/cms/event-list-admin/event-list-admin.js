5bf9968d56c9506b5e9e4f61b3a49e672437d95e
markjangeles10 <markjangeles10@gmail.com>
2017-06-26 02:01:31 +0800

    test

--- www/app/cms/event-list-admin/event-list-admin.js
+++ www/app/cms/event-list-admin/event-list-admin.js
@@ -1,9 +1,26 @@
-app.controller('eventListadminController', function($scope) {
+app.controller('eventListadminController', function($scope, $http, $cookies) {
     $scope.$parent.hideNav = 1;
+	$scope.events = '';
+
     // Check if user is logged
     if ($cookies.get('user_type_id') < 3){
 		//user is correct
 	}else{
 		$location.path('/login');
 	}
+
+	$scope.getAllEvents = function(){
+        $http({
+			method: 'POST',
+			url: api_url+'all_events',
+			data:{
+				'user_id':$cookies.get('user_id'),
+				'hash':$cookies.get('hash')
+			}
+		}).then(function successCallback(response) {
+            $scope.events = response.data.events;
+            console.log($scope.events);
+        });
+    }
+	$scope.getAllEvents();
 });
