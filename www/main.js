36b42382d99bf87c0b12026c6906b7c48bc55ca3
markjangeles10@gmail.com <markjangeles10@gmail.com>
2017-07-02 04:49:18 +0800

    locale-app-logout-update

--- www/main.js
+++ www/main.js
@@ -1,7 +1,24 @@
-app.controller('mainController', function($scope,$location) {
+app.controller('mainController', function($scope, $location, $http, $cookies) {
 	$scope.hideNav = 0;
 	$scope.localSettings = false;
 	$scope.localSettings = function() {
 		$scope.localsettingsActive = !$scope.localsettingsActive;
-	};
+	}
+	
+	$scope.logoutAll = function(){
+		$http({
+			method: 'POST',
+			url: api_url+'logout',
+			data:{
+				'user_id':$cookies.get('user_id')
+			}
+		}).then(function successCallback(response) {
+
+			//delete cookie on logout
+			$cookies.remove('user_id');
+			$cookies.remove('hash');
+			$location.path('/login')
+			$scope.localsettingsActive = !$scope.localsettingsActive;
+		});
+	}
 });
